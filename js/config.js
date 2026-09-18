26a693683fe53bfaf24470a8a7a6de94eb6314d5
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 18:09:21 -0300

    Separa configuración editable de Merlina

---
+++ js/config.js
@@ -0,0 +1,49 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Agustina",
+    age: 10,
+    message: "¿Le temes a divertirte un montón? ¡Te espero!",
+    eventDateTime: "2026-10-18T16:00:00-03:00",
+    eventEndDateTime: "2026-10-18T20:00:00-03:00",
+    dateLabel: "Domingo 18 de Octubre",
+    timeLabel: "16:00 a 20:00 hs.",
+    venue: "Paloko Bowling",
+    address: "Av. San Martín 2269, CABA",
+    mapUrl: "https://maps.app.goo.gl/LnZVf7kF9egGp7Lk8",
+    dressCode: "Ven vestido de negro.",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Agustina. 🖤",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionMerlina/main/assets/img/character-2.png",
+      width: "min(86vw, 410px)",
+      bottom: "-5px",
+      left: "50%",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionMerlina/main/assets/img/character-1.png",
+      width: "min(95vw, 455px)",
+      bottom: "-8px",
+      left: "50%",
+      translateX: "-52%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionMerlina/main/assets/img/character-3.png",
+      width: "min(92vw, 440px)",
+      bottom: "-4px",
+      left: "50%",
+      translateX: "-49%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionMerlina/main/assets/img/character-4.png",
+      width: "min(102vw, 485px)",
+      bottom: "-12px",
+      left: "50%",
+      translateX: "-50%"
+    }
+  ]
+};
